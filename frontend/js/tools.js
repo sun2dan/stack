63bc1db0d2f33811133a39d5ae4e024daf052734
sunchao <sunchao5@jd.com>
2018-11-06 07:25:15 +0800

    fix: 修复format相关的一些bug

--- frontend/js/tools.js
+++ frontend/js/tools.js
@@ -27,17 +27,23 @@ window.tools = {
     },
 
     formatDate: function (date) {
-        date = date || new Date();
-        var str = date.toString(); // Sun Apr 01 2018 21:57:48 GMT+0800 (CST)
-        var res = str.replace(/\s+/gmi, "").replace(/^[a-z]+/gmi, "").replace(/G[a-z\+\(\)\d]+/gmi, "").replace(/(\d{2})(\d{4})([\d:]+)/gmi, "$2--$1 $3");
+      date = date || new Date();
+      var str = date.toString(); // Sun Apr 01 2018 21:57:48 GMT+0800 (CST)
 
-        var m = (date.getMonth() + 1).toString().replace(/^(\d)$/, "-0$1-");
-        res = res.replace(/--/gmi, m);
-        return res;
-    },
+      // 一个长长的正则，匹配出所有需要的数据
+      var res = str.replace(/\w+\s+(\w+)\s+(\d{2})\s+(\d{4})\s+(\d{2}):(\d{2}):(\d{2})[\s\S]+/gmi, "$3--$2 $4:$5:$6");  // 2018--04-01 12:04:05
+
+      // 多个简短的正则：去掉空格，头部字母，结尾字母，格式化除月之外的数据
+      // var res = str.replace(/\s+/gmi, "").replace(/^[a-z]+/gmi, "").replace(/G[a-z\+\(\)\d]+/gmi, "").replace(/(\d{2})(\d{4})([\d:]+)/gmi, "$2--$1 $3");
+
+      // 月份是英文缩写，直接用 getMonth 方法获取到
+      var m = (date.getMonth() + 1).toString().replace(/^(\d)$/, "0$1").replace(/^(\d+)$/gmi, '-$1-');
+      res = res.replace(/--/gmi, m);
+      return res;
+    },                          
 
     formatAmount: function (amo) {
-        amo = amo || "";
-        return amo.toLocaleString("en_us");
+        amo = amo || 0;
+        return amo.toLocaleString("en-US");
     }
 };
