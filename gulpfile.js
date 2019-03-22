dd3672b7713994c990774caf97e5bb9548170c01
sunchao <sunchao5@jd.com>
2019-03-22 16:02:37 +0800

    refactor: 所有远端脚本都拿到本地，cdn有些不靠谱

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,17 @@
 var gulp = require('gulp');
-var path = require('path');
-var fs = require('fs');
-
-// Plugins
-var uglify = require('gulp-uglify');
-var minifyCSS = require('gulp-minify-css');
-var wrapper = require('gulp-wrapper');
-var replace = require('gulp-replace');
-var foreach = require('gulp-foreach');
-var fileinclude = require('gulp-file-include');
-var rename = require("gulp-rename");
-
 var browserSync = require('browser-sync');
-var remoteSrc = require('gulp-remote-src');
-var gutil = require('gulp-util');
-var ftp = require('vinyl-ftp');
-var gulpSequence = require('gulp-sequence');  //管理任务队列
-var transport = require("gulp-seajs-transport");
 
-/**================================================================
- Static server
- =================================================================*/
 gulp.task('server', function () {
-    var files = [
-        '**/**/*.*'
-    ];
-    browserSync.init(files, {
-        server: {
-            baseDir: './',
-            directory: true,
-            https: false
-        }
-    });
+  var files = [
+    '**/**/*.*'
+  ];
+  browserSync.init(files, {
+    server: {
+      baseDir: './',
+      directory: true,
+      https: false
+    },
+    port: 3002
+  });
 });
+
